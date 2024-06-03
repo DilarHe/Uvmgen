5264b2ed46aa72e25e15aff012c2bf6b4db02cf0
Dilar He <hewc96@outlook.com>
2024-06-03 11:05:38 +0800

    fix some bugs remove monitor in agent update filelist, make file and readme

--- generate_uvm_testbench.py
+++ generate_uvm_testbench.py
@@ -1,9 +1,11 @@
 import re
 import argparse
 from mako.template import Template
+
 def remove_line_comments(verilog_code):
     # Remove line comments that start with // and extend to the end of the line
-    return re.sub(r'//.*','',verilog_code)
+    return re.sub(r'//.*', '', verilog_code)
+
 
 def parse_verilog_module(verilog_code):
     verilog_code = remove_line_comments(verilog_code)
@@ -27,23 +29,25 @@ def parse_verilog_module(verilog_code):
     return module_name, parsed_ports
 
 
-def generate_uvm_testbench(verilog_code):
+def generate_uvm_testbench(verilog_code, template):
     module_name, ports = parse_verilog_module(verilog_code)
-
-    templates = {
-        "sequence_item.svh": "./template/sequence_item.mako",
-        "sequence.svh": "./template/sequence.mako",
-        "driver.svh": "./template/driver.mako",
-        "monitor.svh": "./template/monitor.mako",
-        "env.svh": "./template/env.mako",
-        "test.svh": "./template/test.mako",
-        "tb_top.sv": "./template/top_module.mako",
-        "virtual_if.svh": "./template/virtual_if.mako",
-        "uvm_package.sv": "./template/uvm_package.mako",
-        "agent.svh": "./template/agent.mako",
-        "sequencer.svh": "./template/sequencer.mako"
-
-    }
+    print(template.split(".")[0])
+    if template:
+        templates = {template: "./template/"+template.split('.')[0]+".mako"}
+    else:
+        templates = {
+            "sequence_item.svh": "./template/sequence_item.mako",
+            "sequence.svh": "./template/sequence.mako",
+            "driver.svh": "./template/driver.mako",
+            "monitor.svh": "./template/monitor.mako",
+            "env.svh": "./template/env.mako",
+            "test.svh": "./template/test.mako",
+            "tb_top.sv": "./template/top_module.mako",
+            "virtual_if.svh": "./template/virtual_if.mako",
+            "uvm_package.sv": "./template/uvm_package.mako",
+            "agent.svh": "./template/agent.mako",
+            "sequencer.svh": "./template/sequencer.mako"
+            }
 
     for filename, template_path in templates.items():
         with open(template_path, "r") as template_file:
@@ -51,17 +55,18 @@ def generate_uvm_testbench(verilog_code):
             rendered = template.render(module_name=module_name, ports=ports)
             with open(filename, "w") as output_file:
                 output_file.write(rendered)
+                print('{0:<20}'.format(filename)+"generated")
 
 
 def main():
-    #parser = argparse.ArgumentParser(description="Generate UVM testbench from Verilog module")
-    #parser.add_argument("-file", type=str, required=True, help="Path to the Verilog module file")
-    #args = parser.parse_args()
-    args_file = "mdio_master.sv"
-    with open(args_file, "r") as verilog_file:
+    parser = argparse.ArgumentParser(description="Generate UVM testbench from Verilog module")
+    parser.add_argument("-file", type=str, required=True, help="Path to the Verilog module file")
+    parser.add_argument("-template", type=str, required=False, help="specified template file")
+    args = parser.parse_args()
+    with open(args.file, "r") as verilog_file:
         verilog_code = verilog_file.read()
 
-    generate_uvm_testbench(verilog_code)
+    generate_uvm_testbench(verilog_code, args.template)
 
 
 if __name__ == "__main__":
